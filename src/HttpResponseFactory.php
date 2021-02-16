0152414a46eaf93e567f61b6994a6ece447d53e3
Christian Nastasi <christian.nastasi@gmail.com>
2021-02-16 09:14:36 +0100

    Extracted HttpResponseFactory interface

--- src/HttpResponseFactory.php
+++ src/HttpResponseFactory.php
@@ -4,38 +4,11 @@ declare(strict_types=1);
 
 namespace CNastasi\JsonApi;
 
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamFactoryInterface;
 
-class HttpResponseFactory
+interface HttpResponseFactory
 {
-    private ResponseFactoryInterface $responseFactory;
-    private StreamFactoryInterface $streamFactory;
+    public function make(string $content, array $headers, int $statusCode = 200): ResponseInterface;
 
-    public function __construct(ResponseFactoryInterface $responseFactory, StreamFactoryInterface $streamFactory)
-    {
-        $this->responseFactory = $responseFactory;
-        $this->streamFactory = $streamFactory;
-    }
-
-    public function make(string $content, array $headers, int $statusCode = 200): ResponseInterface
-    {
-        $response = $this->responseFactory->createResponse($statusCode);
-
-        foreach ($headers as $key => $value) {
-            $response = $response->withHeader($key, $value);
-        }
-
-        return $response->withBody($this->streamFactory->createStream($content));
-    }
-
-    public function json(array $body,array $headers = [], int $statusCode = 200, string $contentType = 'application/json'): ResponseInterface
-    {
-        $headers = array_merge($headers, ['Content-Type' => $contentType]);
-
-        $content = \json_encode($body, JSON_THROW_ON_ERROR);
-
-        return $this->make($content, $headers, $statusCode);
-    }
+    public function json(array $body, array $headers = [], int $statusCode = 200, string $contentType = 'application/json'): ResponseInterface;
 }

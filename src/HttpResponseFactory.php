42869da46afbf705a46b560a39f9a66ce59a19a4
Christian Nastasi <christian.nastasi@gmail.com>
2021-03-17 12:57:31 +0100

    Add stream to HttpResponseFactory

--- src/HttpResponseFactory.php
+++ src/HttpResponseFactory.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace CNastasi\JsonApi;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 interface HttpResponseFactory
 {
     public function make(string $content, array $headers, int $statusCode = 200): ResponseInterface;
 
     public function json(array $body, array $headers = [], int $statusCode = 200, string $contentType = 'application/json'): ResponseInterface;
+
+    public function stream(StreamInterface $stream, array $headers = [], int $statusCode = 200): ResponseInterface;
 }
--- src/HttpResponseFactoryImpl.php
+++ src/HttpResponseFactoryImpl.php
@@ -7,6 +7,7 @@ namespace CNastasi\JsonApi;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
 
 class HttpResponseFactoryImpl implements HttpResponseFactory
 {
@@ -21,13 +22,7 @@ class HttpResponseFactoryImpl implements HttpResponseFactory
 
     public function make(string $content, array $headers, int $statusCode = 200): ResponseInterface
     {
-        $response = $this->responseFactory->createResponse($statusCode);
-
-        foreach ($headers as $key => $value) {
-            $response = $response->withHeader($key, $value);
-        }
-
-        return $response->withBody($this->streamFactory->createStream($content));
+        return $this->stream($this->streamFactory->createStream($content), $headers, $statusCode);
     }
 
     public function json(array $body,array $headers = [], int $statusCode = 200, string $contentType = 'application/json'): ResponseInterface
@@ -37,5 +32,16 @@ class HttpResponseFactoryImpl implements HttpResponseFactory
         $content = \json_encode($body, JSON_THROW_ON_ERROR);
 
         return $this->make($content, $headers, $statusCode);
+    }
+
+    public function stream(StreamInterface $stream, array $headers = [], int $statusCode = 200): ResponseInterface
+    {
+        $response = $this->responseFactory->createResponse($statusCode);
+
+        foreach ($headers as $key => $value) {
+            $response = $response->withHeader($key, $value);
+        }
+
+        return $response->withBody($stream);
     }
 }
